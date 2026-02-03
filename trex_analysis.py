655903e746f8a814e6a5ad6cc742b6b991390003
Michael Bass <littlebass09@tamu.edu>
2026-02-02 23:38:04 -0800

    MB Initial push

---
+++ trex_analysis.py
@@ -0,0 +1,26 @@
+
+
+
+### Kits that willl be helpful - Data, View, Join, Filter, ColumnOp, DataframePivot
+### The focus of this challenge is join tables, filtering the joined table,
+###    doing an operation, and summarizing a statistic
+
+## We will determine which region provides the cuddly T-Rex
+##    with the most meat per minute, on average, during a hunt!
+
+
+# Load the trex_profiles.csv
+
+# Load the feeding_events.csv
+
+# Join the tables on the T-Rex's name
+
+# We only want to look at events where the hunt was a success
+
+# Compute the amount of meat captured from a hunt per minute of hunting
+
+# Pivot the data to see the average of the previously computed value per territory
+
+# Submit the territory that provides the maximum average of the computed meat per minute
+
+
